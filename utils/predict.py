857e562342d55a3e290de6c39afb7cc6c21593a5
Nirmal Pandey <nirmalpandey048@gmail.com>
2025-03-28 22:53:51 -0400

    Updated model choices for models

--- utils/predict.py
+++ utils/predict.py
@@ -1,7 +1,7 @@
 import numpy as np 
-from utils.data_loader import lr_model
+from utils.data_loader import lr_model, dt_model, rf_model, xgb_model, nb_model
 
-def predict_mental_health_risk( age, gender, relationship_status, occupation_status,
+def predict_mental_health_risk( model_choice, age, gender, relationship_status, occupation_status,
                                 use_social_media, daily_social_media_time, Discord,
                                 Facebook, Instagram, Pinterest, Reddit, Snapchat,
                                 TikTok, Twitter, YouTube):
@@ -25,5 +25,24 @@ def predict_mental_health_risk( age, gender, relationship_status, occupation_sta
             Twitter,
             YouTube
 ])
-    prediction=lr_model.predict([features_array])
+    if model_choice == 'Logistic Regression':
+        prediction = lr_model.predict([features_array])
+        
+    elif model_choice == 'Decision Tree':
+        prediction = dt_model.predict([features_array])
+        
+
+    elif model_choice == 'Random Forest':
+        prediction = rf_model.predict([features_array])
+    
+    elif model_choice == 'XGBoost':
+        prediction = xgb_model.predict([features_array])
+        
+    elif model_choice == 'Naive Bayes':
+        prediction = nb_model.predict([features_array])
+        
+    else:
+        return None
+       
+    
     return prediction
