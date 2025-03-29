8d96d410bf5ae6f3b4e6db9dbfc07f943d4c8ba5
Nirmal Pandey <nirmalpandey048@gmail.com>
2025-03-28 22:54:18 -0400

    Update with new model select choices

--- utils/confusion_matrix.py
+++ utils/confusion_matrix.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st 
 from sklearn.metrics import confusion_matrix
+from utils.data_loader import lr_model, dt_model, dt_model, rf_model, xgb_model, nb_model
 
 
 def plot_confusion_matrix(y_true, y_pred, classes):
@@ -10,13 +11,10 @@ def plot_confusion_matrix(y_true, y_pred, classes):
     Create a heatmap-style confusion matrix for Streamlit
 
     """
-    # Compute confusion matrix
     cm = confusion_matrix(y_true, y_pred)
     
-    # Create a figure and set its size
     plt.figure(figsize=(8, 6))
     
-    # Create heatmap using Seaborn
     sns.heatmap(cm, 
                 annot=True,  # Show numerical values in each cell
                 fmt='d',     # Integer formatting
@@ -24,27 +22,46 @@ def plot_confusion_matrix(y_true, y_pred, classes):
                 xticklabels=classes,
                 yticklabels=classes)
     
-    # Set labels and title
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
     plt.title('Confusion Matrix')
     
-    # Adjust layout and return the plot
     plt.tight_layout()
-    
-    # Return the plot to be used in Streamlit
+       
     return plt
 
-def display_confusion_matrix(model, X_test, y_test):
+def display_confusion_matrix(model_choice, X_test, y_test):
     """
     Display confusion matrix in Streamlit
     
     """
-    # Make predictions
-    y_pred = model.predict(X_test)
+    if model_choice == 'Logistic Regression':
+        y_pred = lr_model.predict(X_test)
+          # Get classes from the model
+        classes = lr_model.classes_
+        
+    elif model_choice == 'Decision Tree':
+        y_pred = dt_model.predict(X_test)
+        classes = dt_model.classes_
+        
+        
+    elif model_choice == 'Random Forest':
+        y_pred = rf_model.predict(X_test)
+        classes = rf_model.classes_
+        
+    elif model_choice == 'XGBoost':
+        y_pred = xgb_model.predict(X_test)
+        classes = xgb_model.classes_
+        
+    elif model_choice == 'Naive Bayes':
+        y_pred = nb_model.predict(X_test)
+        classes = nb_model.classes_
+        
+    else:
+        return None
+
     
-    # Get classes from the model
-    classes = model.classes_
+  
     
     # Create the confusion matrix plot
     st.subheader("Confusion Matrix Visualization")
